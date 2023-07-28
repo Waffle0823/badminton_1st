c8c5214c9f372514a2fa5c4fbec932c29a0b8ac5
Waffle0823 <csshin9928@gmail.com>
2023-07-28 15:42:08 +0900

    [Members] Modal Design

--- public/javascript/pages/members.js
+++ public/javascript/pages/members.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    $('#btn_delete').click(function(){
+        let email = $("#delete_email").val();
+
+        delMembers(email);
+    });
+
     getMembers();
 });
 
@@ -26,12 +32,40 @@ function getMembers() {
             "columnDefs": [ {
                 "targets": 5,
                 "render": function ( data, type, row, meta ) {
-                    return `<span class="btn btn-primary-outline material-icons" style="background-color:transparent; color:red;">
+                    return `<span class="btn btn-primary-outline material-icons btn_delete" style="background-color:transparent; color:red;" data-bs-toggle="modal" data-bs-target="#myModal">
                     delete
                     </span>`;
                 }
-              } ]
+            }]
         });
+
+        $('.btn_delete').click(function(){
+            let email = $(this).closest("tr").children().eq(0).text();
+
+            $("#delete_email").val(email)
+        });
+    })
+    // HTTP 요청이 실패하면 오류와 상태에 관한 정보가 fail() 메소드로 전달됨.
+    .fail(function(xhr, status, errorThrown) {
+        console.log('error: ', errorThrown, status);
+    })
+    // 
+    .always(function(xhr, status) {
+        console.log('always message');
+    });
+}
+
+
+function delMembers(email) {
+    $.ajax({
+        url: "/members", // 클라이언트가 HTTP 요청을 보낼 서버의 URL 주소
+        data: { email: email },  // HTTP 요청과 함께 서버로 보낼 데이터
+        method: "DELETE"   // HTTP 요청 메소드(GET, POST 등)
+        // dataType: "json" // 서버에서 보내줄 데이터의 타입
+    })
+    // HTTP 요청이 성공하면 요청한 데이터가 done() 메소드로 전달됨.
+    .done(function(json) {
+        document.location.reload();
     })
     // HTTP 요청이 실패하면 오류와 상태에 관한 정보가 fail() 메소드로 전달됨.
     .fail(function(xhr, status, errorThrown) {
