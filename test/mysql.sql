c7298804aad71b38bf60acaaefbec1df06bd1ec0
Waffle0823 <csshin9928@gmail.com>
2023-07-29 18:23:44 +0900

    [DB] Add sample query for table 'history'.

--- test/mysql.sql
+++ test/mysql.sql
@@ -17,3 +17,19 @@ CREATE TABLE member_table (
 
 INSERT INTO member_table (email, pw, address, name, phone)
     VALUE('test1@gmail.com', 'test1', 'test1', 'test1', '01012341231');
+
+
+
+DROP TABLE  IF EXISTS member_table;
+
+CREATE TABLE history_table (
+    seq         INT NOT NULL AUTO_INCREMENT,
+    email       VARCHAR(20),
+    path     VARCHAR(256),
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    delete_date TIMESTAMP,
+    PRIMARY KEY(seq)
+) ENGINE=MYISAM CHARSET=utf8;
+
+INSERT INTO history_table (email, path)
+    VALUE('test1@gmail.com', 'test1');
